3742842c772a0648f9936e3c01794ce35c60051c
aritra_iitkgp <aritra.boseiitkgp@outlook.com>
2026-08-06 02:29:51 +0530

    changes

--- src/app.py
+++ src/app.py
@@ -13,7 +13,11 @@ load_dotenv()
 app = Flask(__name__,
             template_folder='../templates', 
             static_folder='../static')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@host.docker.internal/ibm_elytespark'
+database_url = os.environ.get("DATABASE_URL")
+
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url or'mysql+pymysql://root:@host.docker.internal/ibm_elytespark'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = os.environ.get("SECRET_KEY")
 db = SQLAlchemy(app)
